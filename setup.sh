6ac4bff176a9c3c13d86aa81b359d13d74ebcfa8
Vadim Ovcearenco <wadim@veles-soft.com>
2018-11-22 10:31:22 +0000

    Add git autocomplete

--- setup.sh
+++ setup.sh
@@ -9,4 +9,10 @@ ln -s $HOME/devenv/.profile $HOME/.profile
 rm /etc/nginx/nginx.conf
 ln -s $HOME/devenv/nginx/nginx.conf /etc/nginx/nginx.conf
 
+
+# install git autocomplete
+# https://git-scm.com/book/en/v1/Git-Basics-Tips-and-Tricks#Auto-Completion
+mkdir -p /etc/bash_completion.d/
+curl https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash > /etc/bash_completion.d/git-completion.bash
+
 # Attention: cmake is required to compile vim YCM plugin. You can uninstall it after VIM installed.
